6c0d1ebdf01a24c3296fb2c9932f964651222f00
VahuBali <67645126+VahuBali@users.noreply.github.com>
2021-03-25 15:49:01 -0400

    Added Ping

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const command = require("./commands")
 // const config = require("./config.json")
 
 client.on('ready', () => {
     console.log('The client is ready!')
+
+    command(client, 'ping', message => {
+        message.channel.send('Pong!')
+    })
+
 })
 
 client.login(process.env.BOT_TOKEN_JS)
